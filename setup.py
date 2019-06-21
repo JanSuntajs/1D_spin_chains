b82563189d316aba77cbe1120768cd976931bd0f
Jan Suntajs <jansuntajs@gmail.com>
2019-06-21 12:15:16 +0200

    changing setup.py structure

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-setup(name='spin_chains_1D',
+setup(name='spins1d',
       version='1.0.0',
       description='A module for calculations with 1D spin chains',
       url='https://github.com/JanSuntajs/1D_spin_chains',
