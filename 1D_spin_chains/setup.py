2aa5245fcdac7346fd3d8aa32c5d7dfd4e3b23d0
Jan Suntajs <jansuntajs@gmail.com>
2019-06-21 11:25:15 +0200

    modifying setup.py

--- 1D_spin_chains/setup.py
+++ 1D_spin_chains/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-setup(name='1D_spin_chains',
+setup(name='spin_chains_1D',
       version='1.0.0',
       description='A module for calculations with 1D spin chains',
       url='https://github.com/JanSuntajs/1D_spin_chains',
